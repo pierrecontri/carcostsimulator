584dc351bf570b5419a3aa6c34e76d5c884ba392
Pierre Contri <pierrecontri@outlook.com>
2017-11-01 19:52:16 +0100

    Py3 migration next

--- compile2.ps1
+++ compile2.ps1
@@ -1,4 +1,4 @@
-python -c "import py_compile; py_compile.compile('src/modules/CarModelized.py', 'bin/modules/CarModelized.pyc')"
+python -c "import py_compile; py_compile.compile('src/modules/Modelizing/CarModelized.py', 'bin/modules/CarModelized.pyc')"
 python -c "import py_compile; py_compile.compile('src/modules/XmlCarManage.py', 'bin/modules/XmlCarManage.pyc')"
 python -c "import py_compile; py_compile.compile('src/CalculVoiture.py', 'bin/CalculVoiture.pyc')"
 xcopy /y src\HMI\CalculVoiture.glade bin\HMI\CalculVoiture.glade
--- src/modules/Modelizing/__init__.py
+++ src/modules/Modelizing/__init__.py
@@ -1,7 +1,8 @@
 # __init__.py
 
-from CarModelized import *
-from FuelModelized import *
-from DriverModelized import *
+from .ObjectModelized import *
+from .CarModelized import *
+from .FuelModelized import *
+from .DriverModelized import *
 
 __all__ = ["XmlExp", "ListManaged", "CarModelized", "Car", "Wearpart", "CostKm", "FuelModelized", "Fuel", "Driver", "Drivertype"]
